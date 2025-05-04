2f35b812f27a9f7df215b5414a113659dcbce010
Sébastien Ternest <sebt-max@blocked.local>
2025-05-04 20:50:38 +0200

    test netl5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url:import.meta.env['NG_APP_BASE_URL'],
-  mapboxToken: import.meta.env['mapboxToken']
+  url: import.meta.env['NG_APP_BASE_URL'],
+  mapboxToken: import.meta.env['NG_APP_MAPBOX_TOKEN']
 };

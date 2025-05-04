0b060fab04e49df4d5183a26a3e4fb6ef0a9a353
Sébastien Ternest <sebt-max@blocked.local>
2025-05-04 17:57:51 +0200

    .env builder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  url:process.env['NG_APP_BASE_URL'],
+  mapboxToken: "pk.eyJ1Ijoic2p0cjIwMjAiLCJhIjoiY2xra3poeDYzMG83YTN1cGN3bWJ0c3duaiJ9.CbZdLELdC_ITA4VddJJPPw"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  mapboxToken: "pk.eyJ1Ijoic2p0cjIwMjAiLCJhIjoiY2xra3poeDYzMG83YTN1cGN3bWJ0c3duaiJ9.CbZdLELdC_ITA4VddJJPPw"
+  url:process.env['NG_APP_BASE_URL'],
+  mapboxToken:process.env['mapboxToken'],
 
 };

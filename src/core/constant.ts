1ec00c157f415bd69c87a889e6ea7e95f8b4561f
Sébastien Ternest <sebt-max@blocked.local>
2025-05-05 18:09:32 +0200

    const API_URL

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -1,5 +1,3 @@
-import {environment} from '../environments/environment';
+import { environment } from '../environments/environment';
 
-export const API_URL = environment.production
-  ? 'https://autoecole-production.up.railway.app/api/v1/'
-  : 'http://localhost:8080/api/v1/';
+export const API_URL = environment.apiUrl;
